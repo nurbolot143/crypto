0c50e64a627a93d48543b361b8d68ddc8728c545
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-06-02 00:33:07 +0300

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+import "./scss/App.scss";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* <Routes>
+          <Route path="/about" element={<Header />} />
+        </Routes> */}
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import logo from "../assets/images/logo.png";
+import logoDark from "../assets/images/logoDark.png";
+import burger from "../assets/icons/burger.svg";
+import closeBtn from "../assets/icons/closeBtn.svg";
+
+const navItems = [
+  { id: 1, label: "home", to: "/" },
+  { id: 2, label: "resources", to: "/resources" },
+  { id: 3, label: "team", to: "/team" },
+  { id: 4, label: "features", to: "/features" },
+  { id: 5, label: "community", to: "/community" },
+  { id: 6, label: "downloads", to: "/downloads" },
+  { id: 7, label: "contuct us", to: "/contuctUs" },
+];
+
+const Header = () => {
+  const [burgerActive, setBurgerActive] = useState(true);
+
+  return (
+    <header className={burgerActive ? "header" : "header header_active"}>
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="logo header__logo">
+            <Link to="/" onClick={() => setBurgerActive(true)}>
+              <img src={burgerActive ? logo : logoDark} alt="Logo" />
+            </Link>
+          </div>
+
+          <div className={burgerActive ? "nav" : "nav nav_active"}>
+            <ul className="nav__list">
+              {navItems.map(({ id, label, to }) => {
+                return (
+                  <li key={id} className="nav__item">
+                    <NavLink
+                      to={to}
+                      className={({ isActive }) =>
+                        "nav__link" + (!isActive ? "" : " nav__link_active")
+                      }
+                      onClick={() => setBurgerActive(true)}
+                    >
+                      {label}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div
+            className="header__burger"
+            onClick={() => {
+              setBurgerActive(!burgerActive);
+            }}
+          >
+            <img src={burgerActive ? burger : closeBtn} alt="menuBtn" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
